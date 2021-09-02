1db8a4cdd549c8974f601c629a19dc0712de95ef
linolee <linolee@naver.com>
2021-09-02 10:24:07 +0900

    2. 목록 표시하기 https://angular.kr/tutorial/toh-pt2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,19 @@ export class HeroesComponent implements OnInit {
 
   constructor() { }
 
+  selectedHero?: Hero;
   hero: Hero = {
     id: 11,
     name: 'Windstorm',
   };
 
+  heroes: Hero[] = [];
+
   ngOnInit(): void {
+    this.heroes = HEROES;
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
