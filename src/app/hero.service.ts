801108839e7782a4bf179ab096dd527922c12b29
linolee <linolee@naver.com>
2021-09-02 20:46:31 +0900

    6. 서버에서 데이터 받아오기 https://angular.kr/tutorial/toh-pt6

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,83 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
-  getHeroes() : Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
-    return heroes;
+  private heroesUrl: string = 'api/heroes';
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(hero => hero.id === id)!;
-    this.messageService.add(`HeroService: fetch hero id = ${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  searchHero(term: string): Observable<Hero[]> {
+    if (!term.trim()) return of([]);
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+      .pipe(tap(
+        x => x.length 
+          ? this.log(`found heroes matching ${term}`) 
+          : this.log(`no heroes matching ${term}`)),
+          catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(tap(_ => this.log(`update hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero')));
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero')));
+  }
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions)
+      .pipe(tap(_ => this.log(`delete hero id = ${id}`)),
+        catchError(this.handleError<Hero>('deleteHero')));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: 리모트 서버로 에러 메시지 보내기
+      console.error(error); // 지금은 콘솔에 로그를 출력합니다.
+
+      // TODO: 사용자가 이해할 수 있는 형태로 변환하기
+      this.log(`${operation} failed: ${error.message}`);
+
+      // 애플리케이션 로직이 끊기지 않도록 기본값으로 받은 객체를 반환합니다.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
